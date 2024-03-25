372af2f924b751c8c009ecd5906faf66ab558c10
majidanasrin <147602095+majidanasrin@users.noreply.github.com>
2024-03-25 15:22:14 +0530

    Update linkedlistmain.cpp

--- linkedlistmain.cpp
+++ linkedlistmain.cpp
@@ -1,52 +1,51 @@
 #include "linkedlist.cpp"
 
 int main(){
-	//Creating first Linked List
-	LinkedList l1;
-	l1.create_LL();
+	
+	LinkedList x1;
+	x1.create_LL();
 	cout<<"Created Linked List 1"<<endl;
-	l1.display();
+	x1.display();
 
-	l1.insert_at_begin(1);
-	l1.insert_at_end(100);
+	x1.insert_at_begin(1);
+	x1.insert_at_end(100);
 	cout<<"Linked List after insertion at beginning and end"<<endl;
-	l1.display();
+	x1.display();
 
-	l1.delete_at_begin();
-	l1.delete_at_end();
+	x1.delete_at_begin();
+	x1.delete_at_end();
 	cout<<"Linked List after deletion at end and beginning"<<endl;
-	l1.display();
+	x1.display();
 
 	cout<<"Searching for a key in Linked List"<<endl;
-	l1.search(20);
+	x1.search(20);
 	
 	cout<<"Reverse of Linked List 1"<<endl;
-	l1.reverse_list();
-	l1.display();
+	x1.reverse_list();
+	x1.display();
 
-	//Creating second linked list
-	LinkedList l2;
-	l2.create_LL();
+	LinkedList x2;
+	x2.create_LL();
 	cout<<"Created Linked List 2"<<endl;
-	l2.display();
+	x2.display();
 	
-	l2.insert_after_key(4,2);
-	l2.insert_before_key(3,9);
+	x2.insert_after_key(4,2);
+	x2.insert_before_key(3,9);
 	cout<<"Linked List after insertion before and after certain keys"<<endl;
-	l2.display();
+	x2.display();
 
-	l2.delete_key(3);
+	x2.delete_key(3);
 	cout<<"Linked List after deleting a key"<<endl;
-	l2.display();
+	x2.display();
 
     cout<<"Sorting Linked List 2"<<endl;
-	l2.sort();
-	l2.display();
+	x2.sort();
+	x2.display();
 
     cout<<"Concatinated Linked List 1 with 2 :"<<endl;
-	l1.concat_lists(l2);
-	l1.display();
+	x1.concat_lists(l2);
+        x1.display();
 
 	cout<<"Reverse traversal of Linked List 2"<<endl;
-	l2.reverse_traversal();
+	x2.reverse_traversal();
 }
